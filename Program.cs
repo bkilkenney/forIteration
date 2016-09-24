447ebed588ad6241f1223ccc026ef4150b4a1b61
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-24 00:14:22 -0400

    For Loop Lesson

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    if (i == 7)
+            //    {
+            //        Console.WriteLine("Lucky number coming!"); //Stopping at something you're looking for, performing an action, then continuing on...
+                     
+            //    }
+            //    Console.WriteLine(i);
+            //    }
+
+            for (int i = 0; i < 12; i++) //keying "for tab-tab" will set up the for loop, can still change variable name and length to suit
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
